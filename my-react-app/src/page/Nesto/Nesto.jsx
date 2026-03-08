3088b71b9626f4a68c44183be8ae5eeeccadb701
Suhaib2006 <suhaibkoppath66@gmail.com>
2026-03-08 10:59:21 +0530

    cart btn setup

--- my-react-app/src/page/Nesto/Nesto.jsx
+++ my-react-app/src/page/Nesto/Nesto.jsx
@@ -157,7 +157,22 @@ function Nesto() {
                                             : <p>₹{elem.rate}</p>}
 
                                         <p>{elem.per}</p>
+
                                     </div>
+                                    
+                                        <div className=" btnscart">
+                                            <div className="Plus">
+                                                <svg width={25} height={25} fill='red' xmlns="http://www.w3.org/2050/svg" viewBox="0 0 640 640">
+                                                <path d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/></svg>
+                                            </div>
+                                            <div className="number">
+                                                2
+                                            </div>
+                                            <div className="minus">
+                                                <svg width={25} height={25} fill='green' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                                <path d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z"/></svg>
+                                            </div>
+                                                </div>
                                     {elem.offerRate != null && <div className="Percentage">{discountPercentage(elem.rate,elem.offerRate)}%</div>}
                                 </div>
                             </div>
